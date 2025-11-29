0c8533f58eb34ee7212346b5c2e2fb74004648a8
Gerardo Rosas García <gerogar1083@gmail.com>
2025-11-29 14:16:12 -0700

    Finish app.py script

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+# Crear casillas de verificación
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+# Lógica para el histograma
+if build_histogram:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Lógica para el gráfico de dispersión
+if build_scatter:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
